51335b843b81e1cbbc6a9a14f33eb890f9ba100f
Shinodari <shinodari_bb@hotmail.com>
2022-11-03 22:36:49 +0700

    Change MainForm is MDIParrent Add LoginForm(Designing)

--- PrintSleeveManagement/LoginForm.cs
+++ PrintSleeveManagement/LoginForm.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace PrintSleeveManagement
 {
-    public partial class Form1 : Form
+    public partial class LoginForm : Form
     {
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
         }
     }
 }
