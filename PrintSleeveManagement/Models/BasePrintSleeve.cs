147b80039284530ccf7091a4327ea5166b5a927a
C13.339 <C13.339@C13-339>
2022-11-04 16:34:09 +0700

    Create Receipt Usecase

---
+++ PrintSleeveManagement/Models/BasePrintSleeve.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintSleeveManagement.Models
+{
+    class BasePrintSleeve
+    {
+        public int ItemNo { get; set; }
+        public string PartNo { get; set; }
+        public int Quantity { get; set; }
+    }
+}
