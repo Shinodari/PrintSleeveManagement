8a5c38800a083ebd7cbda3601c09c63dc818a766
shino <shino@Shinodari-PC>
2023-05-25 22:57:40 +0700

    ExpireDate Usecase(OverviewForm)

--- PrintSleeveManagement/OverviewForm.Designer.cs
+++ PrintSleeveManagement/OverviewForm.Designer.cs
@@ -28,15 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBoxExpiredDate = new System.Windows.Forms.GroupBox();
+            this.groupBoxExpired = new System.Windows.Forms.GroupBox();
+            this.groupBoxExpiredDate.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBoxExpiredDate
+            // 
+            this.groupBoxExpiredDate.Controls.Add(this.groupBoxExpired);
+            this.groupBoxExpiredDate.Location = new System.Drawing.Point(13, 13);
+            this.groupBoxExpiredDate.Name = "groupBoxExpiredDate";
+            this.groupBoxExpiredDate.Size = new System.Drawing.Size(830, 478);
+            this.groupBoxExpiredDate.TabIndex = 0;
+            this.groupBoxExpiredDate.TabStop = false;
+            this.groupBoxExpiredDate.Text = "Expired Date Report";
+            // 
+            // groupBoxExpired
+            // 
+            this.groupBoxExpired.ForeColor = System.Drawing.Color.Red;
+            this.groupBoxExpired.Location = new System.Drawing.Point(7, 20);
+            this.groupBoxExpired.Name = "groupBoxExpired";
+            this.groupBoxExpired.Size = new System.Drawing.Size(437, 452);
+            this.groupBoxExpired.TabIndex = 0;
+            this.groupBoxExpired.TabStop = false;
+            this.groupBoxExpired.Text = "Expired";
             // 
             // OverviewForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(1664, 632);
             this.ControlBox = false;
+            this.Controls.Add(this.groupBoxExpiredDate);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "OverviewForm";
@@ -44,10 +68,14 @@
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Activated += new System.EventHandler(this.OverviewForm_Activated);
             this.Load += new System.EventHandler(this.OverviewForm_Load);
+            this.groupBoxExpiredDate.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxExpiredDate;
+        private System.Windows.Forms.GroupBox groupBoxExpired;
     }
 }
